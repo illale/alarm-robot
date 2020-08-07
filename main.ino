6893761ec481544b97948061a5483e3ddca70649
ikarrila <47640841+ikarrila@users.noreply.github.com>
2020-08-07 11:34:11 +0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -27,6 +27,19 @@ const int tiltPin = 11;
 const int ledPin = 10;
 const int audPin = 3;
 
+//Pinnit pyörien moottoreille
+const int rightPinActivate = 4;
+const int rightPin5 = 5;
+const int rightPin6 = 6;
+
+const int leftPinActivate = 7;
+const int leftPin8 = 8;
+const int leftPin9 = 9;
+
+//Servon nollapositio
+int pos = 90;
+Servo servo;
+
 //Lippu, jonka avulla tarkistetaan onko moottori käynnistetty
 bool IS_DRIVING = false;
 bool CONTINUOS = false;
@@ -67,10 +80,42 @@ void check_time() {
     }
 }
 
+//Alustetaan moottori
+void initMotor(int activationPin, int pin1, int pin2)
+{
+    pinMode(activationPin, OUTPUT);
+    pinMode(pin1, OUTPUT);
+    pinMode(pin2, OUTPUT);
+    digitalWrite(activationPin, HIGH); 
+}
+
+//Pyöritetään määrättyä moottoria eteen tai taakse
+void drive(int pin1, int pin2, bool forward = true){
+    if (forward) {
+        //Ajetaan eteenpäin
+        digitalWrite(pin1, HIGH);
+        digitalWrite(pin2, LOW);
+    } else {
+        //Peruutetaan
+        digitalWrite(pin1, LOW);
+        digitalWrite(pin2, HIGH);
+    }
+}
+
+//Pysäyttää liikkeen
+void stop(){
+    digitalWrite(rightPin5, LOW);
+    digitalWrite(rightPin6, LOW);
+    digitalWrite(leftPin8, LOW);
+    digitalWrite(leftPin9, LOW);
+}
+
 void move_machine() {
     //Tässä funktiossa "käynnistetään" moottori ja ajetaan eteenpäin.
     if (!IS_DRIVING) {
         //Start the motor here
+        drive(leftPin8, leftPin9, true);
+        drive(rightPin5, rightPin6, true);
     }
 }
 
@@ -109,11 +154,31 @@ void check_obstacles() {
 
 void init_servo() {
     //Asetetaan servo alkuasentoon
+    servo.write(pos);
 }
 
 void dodge_obstacles() {
-    //Väistetään este kääntämällä "tornia" oikealle ja vasemmale, jonka jälkeen päätetään kumpaan suuntaan käännytään.
+    //Väistetään este kääntämällä "tornia" oikealle ja vasemmalle, jonka jälkeen päätetään kumpaan suuntaan käännytään.
     //Kääntyminen tapahtuu käyttämällä vain jompaa kumpaa moottoria oikeaa tai vasenta.
+    servo.write(140);
+    delay(1000);
+    //long distLeft = getDistance();
+    servo.write(40);
+    delay(1000);
+    long distRight = getDistance();
+
+    if (distLeft <= distRight){
+        //Käännytään oikealle jos vasemmalla este
+        drive(rightPin5, rightPin6, true);
+        delay(2000);
+        stop();
+        
+    } else {
+        //Vasemmalle jos toisin päin
+        drive(leftPin8, leftPin9, true);
+        delay(2000);
+    }
+    servo.write(pos);
 }
 
 void detect_off_signal() {
@@ -127,6 +192,9 @@ void setup() {
     pinMode(tiltPin, INPUT);
     pinMode(ledPin, OUTPUT);
     pinMode(audPin, OUTPUT);
+    initMotor(leftPinActivate, leftPin8, leftPin9);
+    initMotor(rightPinActivate, rightPin5, rightPin6);
+    servo.attach(2);
 }
 
 void loop() {
