2a6d3c80b87c3446ef7916f7e5049b41597b1190
Aleksi Illikainen <illikainen.aleksi@gmail.com>
2020-08-17 21:31:28 +0300

    Päivitin dokumentaatiota

--- main/main.ino
+++ main/main.ino
@@ -3,30 +3,36 @@
 
 
 /*
-IDLE_STATE: Tarkistaa onko kello jo sen verran, milloin hälytys soi, ja kone alkaa ajamaan.
-ALARM_STATE: Kone hälyttää ja ajaa eteenpäin.
-OBSTACLE_STATE: Kone kääntyy väistäen esteen ja jatkaa hälyttämistä.
+IDLE_STATE: In this state, the arduino checks is the current running time higher than the wanted
+            interval
+ALARM_STATE: The robot alarms, tries to escape from the human
+OBSTACLE_STATE: The robot detects obstacle, evades it, and continues to sound the alarm
 
-Tilanmuunnokset:
-IDLE_STATE -> ALARM_STATE, kun aika on asetetun ajan verran.
-ALARM_STATE -> OBSTACLE_STATE, kun huomataan este suoraan edessä.
-ALARM_STATE -> IDLE_STATE, kun konetta heilutetaan, ja hälytys lopetetaan.
-OBSTACLE_STATE -> ALARM_STATE, kun este on väistetty.
-OBSTACLE_STATE -> IDLE_STATE, kun konetta heilutetaan, ja hälytyts lopetetaan.
+State changes:
+IDLE_STATE -> ALARM_STATE, when the running time is higher than the interval
+ALARM_STATE -> OBSTACLE_STATE, when the robot notices obstacle in its way 
+ALARM_STATE -> IDLE_STATE, when the robot is shaken by human, this results in the alarm stopping
+OBSTACLE_STATE -> ALARM_STATE, when the obstacle has been avoided
+OBSTACLE_STATE -> IDLE_STATE, when the robot is shaken by human, this results in the alarm stopping
 */
 enum STATE { IDLE_STATE, ALARM_STATE, OBSTACLE_STATE, SETTINGS_STATE};
-//Asetetaan ensimmäiseksi tilaksi IDLE_STATE
+
+//Set the first state to IDLE_STATE
 enum STATE MACHINE_STATE;
 
-//Pinnit, joita ultraäänisensori käyttää
+//Pins that the ultrasound sensor uses
 const int echoPin = 13;
 const int trigPin = 12;
 
+//Pin used for tilt sensor
 const int tiltPin = 11;
-const int ledPin = 10;
+
+//const int ledPin = 10;
+
+//Pin for the audio device
 const int audPin = 3;
 
-//Pinnit pyörien moottoreille
+//Pins for the motors
 const int rightPinActivate = 4;
 const int rightPin5 = 5;
 const int rightPin6 = 6;
@@ -35,61 +41,72 @@ const int leftPinActivate = 7;
 const int leftPin8 = 8;
 const int leftPin9 = 9;
 
-//Servon nollapositio
+//Zero position of the servo
 int pos = 90;
 Servo servo;
 
-//Lippu, jonka avulla tarkistetaan onko moottori käynnistetty
+//Flag which is used for detecting if the motors has been started
 bool IS_DRIVING = false;
+
+//Flag that is used for continuos alarm:
+//If false, then there is no second alarm, and vice versa
 bool CONTINUOS = false;
-//Oletusarvo ajalle, jonka kuluttua hälytetään millisekuntteina
-long wanted_time_interval = 200000;
-long time = 5000;
+//Default value for time interval in milliseconds
+long time;
+long wanted_time_interval;
 long current_time;
 
 
 void get_settings() {
+    //Check if there is data in serial channel, and if there is no available data set time to default value
     if (Serial.available() > 0) {
-        int bytes[4];
+
+        int bytes[3];
         int byte = 0;
         int i = 0;
         int j = 0;
         long end_values[10];
         while((byte = Serial.read()) != -1) {
-            //Tulevat arvot ovat 32 bittisiä, joten 4 tavua on yksi arvo
+            //Incoming values should be 32 bit values
             bytes[i] = byte;
-            if (i == 3) {
-                //Olettaa että arvojen MSB on vasemmassa laidassa, eli listan ensimmäisen arvon ensimmäinen bitti on MSB
-                end_values[j] = bytes[0] << 24 | bytes[1] << 16 | bytes[2] << 8 | bytes[3];
+            if (i == 1) {
+                //Transform the first two 16 bit values into one 32 bit value
+                //This should be the value for the time interval
+                end_values[j] = bytes[0] << 16 | bytes[1] << 0;
                 j++;
-            } else if (i == 4) {
-              end_values[j] = bytes[i];
-              break;
+            } else if (i == 2) {
+                //This is the final value for the continuos FLAG
+                end_values[j] = bytes[i];
+                break;
             } else {
                 i++;
             }
         }
         time = end_values[0];
         if (end_values[1] == 1) {
-          CONTINUOS = true;
+            CONTINUOS = true;
         }
-        }
-      else {
-        time = 5000;  
+    } else {
+        time = 50;  
       }
+    //Store the wanted time inteval, so that time is never doubled
+    wanted_time_interval = time;
 }
 
 void check_time() {
-    //Tässä funktiossa tarkistetaan aika, ja jos aika == haluttu aika, niin vaihdetaan tilaan ALARM_STATE
+    //Check if the wanted time interval has been crossed, and change the state accodingly
+
+    //Check the current running time
     current_time = millis();
+
     if (time - current_time <= 0 && time != 0) {
         MACHINE_STATE = ALARM_STATE;
-        //Tarkistetaan onko haluttu hälytys ns jatkuva vai pelkästään yhden kerran hälyttävä
+        //Check if alarm is contiunos and add another interval accordingly
         CONTINUOS ? time = 0 : time += wanted_time_interval;
     }
 }
 
-//Alustetaan moottori
+//Initialise the motors
 void initMotor(int activationPin, int pin1, int pin2)
 {
     pinMode(activationPin, OUTPUT);
@@ -98,20 +115,20 @@ void initMotor(int activationPin, int pin1, int pin2)
     digitalWrite(activationPin, HIGH); 
 }
 
-//Pyöritetään määrättyä moottoria eteen tai taakse
+//Spin the motors forward or backwards 
 void drive(int pin1, int pin2, bool forward = true){
     if (forward) {
-        //Ajetaan eteenpäin
+        //Drive forward
         digitalWrite(pin1, HIGH);
         digitalWrite(pin2, LOW);
     } else {
-        //Peruutetaan
+        //Reverse
         digitalWrite(pin1, LOW);
         digitalWrite(pin2, HIGH);
     }
 }
 
-//Pysäyttää liikkeen
+//Stop the motors
 void stop(){
     digitalWrite(rightPin5, LOW);
     digitalWrite(rightPin6, LOW);
@@ -120,7 +137,7 @@ void stop(){
 }
 
 void move_machine() {
-    //Tässä funktiossa "käynnistetään" moottori ja ajetaan eteenpäin.
+     //Turn the motors on if they havent been turned on
     if (!IS_DRIVING) {
         //Start the motor here
         drive(leftPin8, leftPin9, true);
@@ -129,7 +146,7 @@ void move_machine() {
 }
 
 long getDistance() {
-    //Lasketaan etäisyys
+    //Calculate the distance to object in front of the ultrasound sensor
     long duration, distance;
    
     digitalWrite(trigPin, LOW);
@@ -141,17 +158,17 @@ long getDistance() {
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
 
-    // Hakee pulssin keston.
+    //Get the length of the pulse
     duration = pulseIn(echoPin, HIGH);
 
-    // Laskee etäisyyden kuluneen ajan perusteella. 
+    //Calculate the distance based on the time the pulse took
     distance = duration * 0.034 / 2;
 
     return distance;
 }
 
 void check_obstacles() {
-    //Tässä funktiossa tarkistetaan onko koneen edessä esteitä, jos on siirytään tilaan OBSTACLE_STATE.
+    //Check if there are any obstacles infront of the robot and change state accordingly
     int upper_bound, lower_bound;
     upper_bound = 10;
     lower_bound = 0;
@@ -162,13 +179,14 @@ void check_obstacles() {
 }
 
 void init_servo() {
-    //Asetetaan servo alkuasentoon
+    //Initialize the servo to the starting position
     servo.write(pos);
 }
 
 void dodge_obstacles() {
-    //Väistetään este kääntämällä "tornia" oikealle ja vasemmalle, jonka jälkeen päätetään kumpaan suuntaan käännytään.
-    //Kääntyminen tapahtuu käyttämällä vain jompaa kumpaa moottoria oikeaa tai vasenta.
+    //Select the "better" direction for the robot by turning the tower to the left and right
+    //and calculating the distancecs of which the shorter is dismised
+    //The robot is turned with using only left or right motor
     servo.write(140);
     delay(1000);
     long distLeft = getDistance();
@@ -177,13 +195,13 @@ void dodge_obstacles() {
     long distRight = getDistance();
 
     if (distLeft <= distRight){
-        //Käännytään oikealle jos vasemmalla este
+        //Turn to the right if obstacle at left
         drive(rightPin5, rightPin6, true);
         delay(2000);
         stop();
         
     } else {
-        //Vasemmalle jos toisin päin
+        //And to left if vice versa
         drive(leftPin8, leftPin9, true);
         delay(2000);
     }
@@ -195,7 +213,7 @@ void detect_off_signal() {
 }
 
 void setup() {
-    MACHINE_STATE = IDLE_STATE;
+    MACHINE_STATE = SETTINGS_STATE;
     Serial.begin(9600);
     pinMode(echoPin, INPUT);
     pinMode(trigPin, OUTPUT);
@@ -214,13 +232,16 @@ void loop() {
         case ALARM_STATE:
             move_machine();
             check_obstacles();
-            //detect_off_signal();
+            detect_off_signal();
         case OBSTACLE_STATE:
             dodge_obstacles();
-            //detect_off_signal();
+            detect_off_signal();
             MACHINE_STATE = ALARM_STATE;
         case SETTINGS_STATE:
-            get_settings();
+            while ( millis() < 3000) {
+                //Check the setting in the first 30 seconds
+                get_settings();
+            }
             MACHINE_STATE = IDLE_STATE;
     }
 }           
--- programmer.py
+++ programmer.py
@@ -5,6 +5,7 @@ import glob
 
 class MainWindow:
     def __init__(self):
+        #Initialize values to empty
         self.window = tk.Tk()
         self.INDEX = 0
         self.SERIAL = "Test"
@@ -24,66 +25,76 @@ class MainWindow:
         self.time_button()
 
     def list_serials(self):
+        #Create listbox and insert all available serial ports into it
         self.serials = tk.Listbox(self.window)
         self.insert_ports()
         self.serials.pack()
 
     def get_button(self):
+        #Create button that selects the used serial port when pressed
         self.button = tk.Button(self.window, text="Select Serial", command=self.button_command)
         self.button.pack()
 
     def time_button(self):
+        #Button that inserts the wanted time into the arduino board
         self.time_button = tk.Button(self.window, text="Insert", command=self.get_entry_text)
         self.time_button.pack()
     
     def get_entry_text(self):
+        #Get the time written into the input box
         text = self.entry.get()
         if (int(text.split(":")[0]) > 24 ):
             print("Not acceptable value")
         else:
             self.TEXT = text
-        print(self.TEXT)
+        #Calculate the wanted interval into milliseconds
         time = int(self.TEXT.split(":")[0]) * 3600000 + int(self.TEXT.split(":")[1]) * 60000
-        print(time)
+        #Write the interval value to the arduino board
         self.SERIAL.write(time)
 
     def button_command(self):
+        #This is the method that takes care of selecting the serial port
         self.INDEX = self.serials.curselection()
         self.SERIAL = self.serials.curselection()
-        print(self.INDEX)
-        print(self.SERIAL)
-        print(self.PORTS[self.SERIAL[0]])
         self.SERIAL = serial_t = serial.Serial(self.PORTS[self.INDEX[0]])
 
     def insert_ports(self):
+        #This method fills the serial list with all available ports.
         if (len(self.PORTS) > 0):
             for i, port in enumerate(self.PORTS):
                 self.serials.insert(i, port)
         else:
+            #If no available ports are found, then the list is filled with two examples
             self.serials.insert(0, "Serial 1")
             self.serials.insert(1, "Serial 2")
 
     def create_entry(self):
+        #Create input box, that takes time interval to next alaram as input
         self.entry = tk.Entry(self.window, text="21:00")
         self.entry.pack()
 
     def set_continuos(self):
+        #Set boolean value, so that we know if we want continuos alarm
+        #E.g that after first alarm, second alarm is automatically set
+        #with wanted interval
         self.CONTINUOS = not self.CONTINUOS
         print(self.CONTINUOS)
 
     def create_boolean_check(self):
+        #Create checkbox that can be off/on that decides end value for continuos alarm
         self_check = tk.Checkbutton(self.window, onvalue=1, offvalue=0,text="Continuos", command=self.set_continuos)
         self_check.pack()
 
     def list_available_serials(self):
-        """
-        This took inspiration from https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
-        """
-        if sys.platform.startswith('win'):
-            ports = ['COM{}'.format(i + 1) for i in range(256)]
-        elif sys.platform.startswith('linux'):
-            ports = glob.glob('/dev/tty[A-Za-z]*')
+        #This part of the code was 'inspired' from Thomas's answer at
+        #https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
+        if sys.platform.startswith("win"):
+            ports = ["COM{}".format(i + 1) for i in range(256)]
+        elif sys.platform.startswith("linux"):
+            ports = glob.glob("/dev/tty[A-Za-z]*")
 
+        #Try if python can open the given serial port, and if it can
+        #add it to available serials list
         for port in ports:
             try:
                 serial_t = serial.Serial(port)
